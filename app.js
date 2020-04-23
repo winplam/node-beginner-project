0053b55d90f0ca20b1408e230a0c46ce91c48f95
Win Lam <kaito@computer.local>
2020-04-22 20:43:33 -0700

    Add express branch

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var port = 8080
+
+app.use(express.static(__dirname, { index: './index.html' }))
+app.use('/contact-me', express.static('./contact-me.html'))
+app.use('/about', express.static('./about.html'))
+
+app.listen(port, function () {
+  console.log('Example app listening on http://localhost:' + port)
+})
