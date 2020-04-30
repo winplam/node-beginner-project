95492e264245512c9cc568997966fd5bb716fdeb
Win Lam <kaito@computer.local>
2020-04-30 09:31:39 -0700

    Update readme and app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express')
 var app = express()
 var port = 8080
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+app.get('/', (req, res) => {res.sendFile(__dirname + '/index.html')})
+app.get('/about', (req, res) => {res.sendFile(__dirname + '/about.html')})
+app.get('/contact-me', (req, res) => {res.sendFile(__dirname + '/contact-me.html')})
 // app.use(express.static(__dirname, { index: './index.html' }))
-app.use('/contact-me', express.static('./contact-me.html'))
-app.use('/about', express.static('./about.html'))
+// app.use('/about', express.static('./about.html'))
+// app.use('/contact-me', express.static('./contact-me.html'))
 
 app.listen(port, function () {
   console.log('Example app listening on http://localhost:' + port)
